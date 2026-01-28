414b27b2dc131f5c98355840da9ea5e32613f2ad
Leonardo <leonardo_vieira199@hotmail.com>
2026-01-28 13:28:46 -0300

    while/do while

---
+++ src/academy/devdojo/maratonajava/estruturascondicionais/EstruturasRepeticao.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.estruturascondicionais;
+
+public class EstruturasRepeticao {
+
+    public static void main(String[] args) {
+        //while, do while, for
+
+        int count = 1;
+        while (count <= 10) {                           // condição do while deve resultar em valor TRUE - loop até que o valor chegue a 10.
+            System.out.println("while: " + count);
+            count++;
+        }
+
+        System.out.println("###################");
+
+        do {                                            // executado ao menos uma vez. FAÇA ATÉ .....
+            System.out.println("do while: " + count);
+            count++;
+        } while (count <= 20);
+
+        System.out.println("###################");
+
+
+    }
+}
